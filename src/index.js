31c2d439bf91e25afdd85009771cb265a51d74b5
finoriko <finoriko@Hotmail.com>
2020-08-28 02:32:37 +0900

    add Dropdown box

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('검색엔진' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          검색엔진
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="google">google</option>
+            <option value="bing">bing</option>
+            <option value="behance">behance</option>
+            <option value="instagram">instagram</option>
+            <option value="pinterest">pinterest</option>
+          </select>
+        </label>
+        <input type="submit" value="배색생성" />
+      </form>
+    );
+  }
+}
+
 ReactDOM.render(
-    <App />,
+    <FlavorForm />,
   document.getElementById('root')
 );
 
