111dcf219e52eb5b4dac56db47c1adbcb2cccc41
finoriko <finoriko@Hotmail.com>
2020-08-31 01:29:50 +0900

    add axios

--- src/index.js
+++ src/index.js
@@ -38,7 +38,17 @@ class FlavorForm extends React.Component {
     );
   }
 }
-
+class myComponent extends React.Component {
+  componentDidMount() {
+    const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  }
+  render() {
+    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
+  }
+}
 ReactDOM.render(
     <FlavorForm />,
   document.getElementById('root')
