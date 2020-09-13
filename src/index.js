644abbabf19f53c92d76c5a492355194e5510bd0
finoriko <finoriko@Hotmail.com>
2020-09-14 00:28:33 +0900

    Component add

--- src/index.js
+++ src/index.js
@@ -1,56 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class FlavorForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 'coconut'};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('검색엔진' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          검색엔진
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="google">google</option>
-            <option value="bing">bing</option>
-            <option value="behance">behance</option>
-            <option value="instagram">instagram</option>
-            <option value="pinterest">pinterest</option>
-          </select>
-        </label>
-        <input type="submit" value="배색생성" />
-      </form>
-    );
-  }
-}
-class myComponent extends React.Component {
-  componentDidMount() {
-    const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log('This is your data', data));
-  }
-  render() {
-    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
-  }
-}
 ReactDOM.render(
-    <FlavorForm />,
+    <App />,
   document.getElementById('root')
 );
 
